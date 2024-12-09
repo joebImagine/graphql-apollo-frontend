529c02d06d575bea8aaf3fb27f10b030413a3a99
Joe Bates <joebates@jbimagine.com>
2024-12-09 08:38:37 -0800

    continue fleshing out the test pagination for users

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import {gql, useQuery} from '@apollo/client';
-import {useState} from 'react';
 import client from '../lib/apollo-client';
 
 const GET_USERS = gql`
@@ -22,25 +21,45 @@ const GET_USERS = gql`
 `;
 
 export default function Home() {
-  const [cursor, setCursor] = useState(null);
   const { data, loading, error, fetchMore } = useQuery(GET_USERS, {
-    variables: { first: 10, after: cursor },
+    variables: { first: 10 },
     client,
   });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
+    /* use this in conjunction with the global apollo-client merge object */
+//   const loadMore = () => {
+//     if (data.users.pageInfo.hasNextPage) {
+//       fetchMore({
+//         variables: { after: data.users.pageInfo.endCursor },
+//       });
+//     }
+//   };
+
+    /* updateQuery allows for handling updates on a local level */
   const loadMore = () => {
     if (data.users.pageInfo.hasNextPage) {
       fetchMore({
         variables: { after: data.users.pageInfo.endCursor },
-      }).then(({ data: newData }) => {
-        setCursor(newData.users.pageInfo.endCursor);
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          if (!fetchMoreResult) return previousResult;
+
+          return {
+            users: {
+              edges: [
+                ...previousResult.users.edges,
+                ...fetchMoreResult.users.edges,
+              ],
+              pageInfo: fetchMoreResult.users.pageInfo,
+            },
+          };
+        },
       });
     }
   };
-
+    
   return (
     <div>
       <h1>Paginated Users</h1>
